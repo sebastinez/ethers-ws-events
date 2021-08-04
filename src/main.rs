4e27e7f9aa0d51a94e886369a11cb6ab7c235506
Sebastian Martinez <sebastinez@me.com>
2021-08-04 14:51:07 -0300

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,23 @@ use org::{AnchoredFilter, Org};
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Org address
+    // Defining and parsing org address
     let address = "0x797Ff53d759e80193c91ACd486E7742a271DD23E".parse::<Address>()?;
 
+    // Connecting to open Websocket Server, polling every 7 seconds
     let ws = Ws::connect("ws://localhost:8546").await?;
 
-    // connect to the network
+    // Instantiating new Provider
     let client = Provider::new(ws);
 
-    // create the contract object at the address
+    // Instantiating a new Org contract at the defined address
     let org_contract = Org::new(address, client.into());
 
+    // Creating a new Event Filter with the defined event name and a stream to listen to
     let event = org_contract.event_for_name::<AnchoredFilter>("Anchored")?;
     let mut stream = event.stream().await?;
 
+    // Wait for websocket messages on this filter and decode and print them
     while let Some(item) = stream.next().await {
         match item {
             Ok(log) => {
