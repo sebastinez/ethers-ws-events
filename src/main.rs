187820d206bebcead405f37902a83e660f36a36a
Sebastian Martinez <sebastinez@me.com>
2021-08-08 00:09:06 -0300

    Change FilterWatcher for SubscriptionStream

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod org;
+
 use ethers::{
     prelude::StreamExt,
     providers::{Provider, Ws},
@@ -11,7 +12,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Defining and parsing org address
     let address = "0x797Ff53d759e80193c91ACd486E7742a271DD23E".parse::<Address>()?;
 
-    // Connecting to open Websocket Server, polling every 7 seconds
+    // Connecting to open Websocket Server
     let ws = Ws::connect("ws://localhost:8546").await?;
 
     // Instantiating new Provider
@@ -20,11 +21,11 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Instantiating a new Org contract at the defined address
     let org_contract = Org::new(address, client.into());
 
-    // Creating a new Event Filter with the defined event name and a stream to listen to
+    // Creating a new EventStream with a defined event name.
     let event = org_contract.event_for_name::<AnchoredFilter>("Anchored")?;
-    let mut stream = event.stream().await?;
+    let mut stream = event.subscribe().await?;
 
-    // Wait for websocket messages on this filter and decode and print them
+    // Iterate over the EventStream, and print the logs.
     while let Some(item) = stream.next().await {
         match item {
             Ok(log) => {
